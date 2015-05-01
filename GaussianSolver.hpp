a6367bc082a00bbe0196ed6858f17d4e36c51c2a
AbiraDas <adnvc@mst.edu>
2015-05-01 16:09:25 -0500

    added gaussian solver

---
+++ GaussianSolver.hpp
@@ -0,0 +1,16 @@
+// Programmer: Abira Das & Patrick Sullivan
+// Class: CS 5201 (Price)
+//GaussianSolver.hpp
+//implementations for the SubstitutionSolver.h file
+
+
+#include <iostream>
+
+template <class DT>
+Vector<DT> GaussianSolver::operator()(FullMatrix<DT>& a, Vector<DT>& b)
+{
+
+
+
+
+}
